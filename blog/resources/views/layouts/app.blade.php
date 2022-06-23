c82ba3bb13ed3e71070a308bfc61fc971ca9445e
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-23 22:11:51 +0200

    new change

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -5,9 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posty</title>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        colors: {
+                            laravel: "#ef3b2d",
+                        },
+                    },
+                },
+            };
+        </script>
 </head>
 <body>
+    
+</body>
+</html>
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="" class="p-3">Dashboard</a></li>
+            <li><a href="" class="p-3">Post</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">RUTAGENGWA Bruce</a></li>
+            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="{{route('register')}}" class="p-3">Register</a></li>
+            <li><a href="" class="p-3">Logout</a></li>
+        </ul>
+    </nav>
     @yield('content')
-    Hello
 </body>
 </html>
