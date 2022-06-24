05c75b9590e97a6d5ecee73a7dfa65bf2bdaa0dc
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-24 15:37:18 +0200

    new change

---
+++ blog/resources/views/users/posts/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    
+</body>
+</html>
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="flex justify-center">
+    <div class="w-8/12 bg-white p-6 rounded-lg">
+        {{$user->name}}
+    </div>
+</div>
+@endsection
