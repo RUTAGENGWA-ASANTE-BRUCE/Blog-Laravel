fd4e0e2762dd2997bfb0dc0fad8859b3c8bc7b90
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-24 10:30:01 +0200

    new change

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -42,12 +42,12 @@
         <ul class="flex items-center">
             <li><a href="/" class="p-3">Home</a></li>
             <li><a href="{{route('dashboard')}}" class="p-3">Dashboard</a></li>
-            <li><a href="" class="p-3">Post</a></li>
+            <li><a href="{{route('posts')}}" class="p-3">Post</a></li>
         </ul>
 
         <ul class="flex items-center">
             @auth
-            <li><a href="" class="p-3">RUTAGENGWA Bruce</a></li>
+            <li><a href="" class="p-3">{{auth()->user()->name}}</a></li>
             <li>
                 <form action="{{route('logout')}}" class="p-3 inline" method="post">
                     @csrf
