42c9eae30d76574ee6817809cf76e878997bc7af
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-24 09:52:15 +0200

    new change

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
-{
+{   public function __construct(){
+        $this->middleware('guest');
+}
     public function index(){
         return view('auth.register');
     }
