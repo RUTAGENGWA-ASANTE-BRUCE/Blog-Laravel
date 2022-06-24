985f8ffcdc79996d3ecdeb90d2e473f29ba774e5
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-24 09:28:43 +0200

    new change

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,15 @@ use App\Http\Controllers\DashboardController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get("/",function () {
+    return view('home');
+})->name('home');
+
+
 Route::get("/login",[LoginController::class,'index'])->name('login');
 Route::post("/login",[LoginController::class,'store']);
 
+Route::post("/logout",[LogoutController::class,'store'])->name('logout');
 
 Route::get("/register",[RegisterController::class,'index'])->name('register');
 Route::post("/register",[RegisterController::class,'store']);
