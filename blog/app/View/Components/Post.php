ba9fef41a27bbe35ed92d843cd9c1b15f0cb2cf5
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-24 16:32:16 +0200

    new change

--- blog/app/View/Components/Post.php
+++ blog/app/View/Components/Post.php
@@ -11,9 +11,11 @@ class Post extends Component
      *
      * @return void
      */
-    public function __construct()
+    public $post;
+
+    public function __construct($post)
     {
-        //
+        $this->post=$post;
     }
 
     /**
